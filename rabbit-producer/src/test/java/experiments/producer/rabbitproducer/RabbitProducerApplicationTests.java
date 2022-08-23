baf9136dbb6f5477bcec07848caf00be6e8a818d
aleksei.tsopov <aleksei.tsopov@ringit.ee>
2022-08-23 16:02:29 +0300

    init commit
    module for rabbit producer
    module for rabbit consumer

---
+++ rabbit-producer/src/test/java/experiments/producer/rabbitproducer/RabbitProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package experiments.producer.rabbitproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
